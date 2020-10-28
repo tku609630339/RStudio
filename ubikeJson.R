cb151a7c4771ac0705ab54199a31788114abd2ae
tku609630339 <chiawen875@gmail.com>
2020-10-28 11:00:53 +0800

    10/28

--- ubikeJson.R
+++ ubikeJson.R
@@ -12,3 +12,15 @@ sort(x)
 sort(x,decreasing = T)
 #UbikeData <- lapply(jdata$retVal, unlist)
 #UbikeData <- data.frame(do.call("rbind", UbikeData))
+
+
+#install.packages('data.table')
+library(data.table)
+
+download.file("https://tcgbusfs.blob.core.windows.net/blobyoubike/YouBikeTP.json",ubike.json)
+ubike<-fromJSON(ubike.json)
+ubike<-rbindlist(ubike$retVal)
+y<-table(ubike$sarea)
+sort(y)
+sort(y,decreasing = T)
+
