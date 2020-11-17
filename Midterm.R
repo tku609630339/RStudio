dc2121a82ada5058695845a4000b6ba579c07061
tku609630339 <chiawen875@gmail.com>
2020-11-17 23:41:55 +0800

    11/17

--- Midterm.R
+++ Midterm.R
@@ -14,18 +14,19 @@ product <- fromJSON("productNTP.json")
 
 
 print("臺灣地區農產品生產量值(data.frame):")
+year<-c(product$year)
 name<-c(product$crop_name)
 output<-c(product$output)
 price<-c(product$price)
 value<-c(product$output_value)
-i<-data.frame(name=name,output=output,price=price,value=value)
+i<-data.frame(year=year,name=name,output=output,price=price,value=value)
 
 
 
 
 
-i1<-data.frame(name=name,output=output)
-i2<-data.frame(name=name,value=value)
+i1<-data.frame(year=year,name=name,output=output)
+i2<-data.frame(year=year,name=name,value=value)
 i
 i1
 i2
@@ -46,17 +47,20 @@ c
 #資料視覺化ggplot2
 windows()
 library(ggplot2)
+a<-i1[i1$year == "94",]%>%
+  head(20)
 
-pro <- table(i1$name)
-barplot(pro, main="臺灣地區農產品生產量", 
-        xlab="name", ylab="output", border='black',
-        col='blue', ylim=c(0, 5))
+dim(a)
+#name<-a$name
+grades<-table(a$name)
+barplot(grades,horiz=TRUE,xlab="output", ylab="name", col = "lightblue",main="臺灣地區農產品產值", ylim=c(0, 100))
 
-pro1 <- table(i2$name)
-barplot(pro1, main="臺灣地區農產品產值", 
-        xlab="name", ylab="output", border='red',
-        col='green', ylim=c(0, 20))
+barplot(grades,col = c("lightblue","lightcyan", "lavender", "mistyrose",  "cornsilk"),legend.text = a$name,main="臺灣地區農產品產值", ylim=c(0, ))
 
-#輸出csv檔(轉換具BOM的UTF-8)
+#pro1 <- table(i2$name)
+#barplot(pro1, main="臺灣地區農產品產值", 
+#        xlab="name", ylab="output", border='red',
+#        col='green', ylim=c(0, 20))
+#v檔(轉換具BOM的UTF-8)
 b<-as.data.frame(ii)
 write.csv(b,file = "NTP_midterm.csv",fileEncoding = "UTF-8",row.names = F)
