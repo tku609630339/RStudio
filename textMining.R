f225bef120317df063963e35ef9223d346870f55
tku609630339 <chiawen875@gmail.com>
2020-11-18 10:22:36 +0800

    11/18

--- textMining.R
+++ textMining.R
@@ -4,3 +4,8 @@ paste(readLines('a.txt'),collapse = "@@@@$$$$@@@@")
 a<-c('Hello','world','name','is','R')
 a
 paste(a,collapse = ' ')
+text1<-paste(readLines('a.txt'),collapse = " ")
+text1
+gsub(pattern = "\\w",replacement = " ",text1)
+strsplit(text1,' ')
+strsplit(a,' ')
