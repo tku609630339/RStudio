5fe9ed20606b7282ff3aa0333ede747b9172761f
tku609630339 <chiawen875@gmail.com>
2020-12-02 07:45:11 +0800

    12/02,ok

--- tm.R
+++ tm.R
@@ -1,32 +1,33 @@
-readLines('a.txt')
-str(readLines('a.txt'))
-paste(readLines('a.txt'),collapse = "@@@@$$$$@@@@")
-a<-c('Hello','world','name','is','R')
-a
-paste(a,collapse = ' ')
-text1<-paste(readLines('a.txt'),collapse = " ")
-text1
-gsub(pattern = "\\w",replacement = " ",text1)
-strsplit(text1,' ')
-strsplit(a,' ')
-
-
-text
-text2
+s <- readLines('a.txt')
+s[1]
+x=strsplit(s,' ')
+str(x)
+x[[1]]
+x[[1]][2]
+y=unlist(x)
+length(y)
+y <- tolower(y)
+z <- removeWords(y,stopwords())
+nchar(z)>0
+z1 <- z[nchar(z)>0]
+gsub('\\W','',z1)
+tolower(y)
+length(z)
+install.packages('tm')
+install.packages('stringr')
+install.packages('wordcloud')
 library(tm)
 library(stringr)
 library(wordcloud)
 stopwords()
 
-a
-b<-paste(a,collapse = " ")
-b
+poswords <- scan('postive-words.txt',what='character')
+poswords1 <- readLines('postive-words.txt')
+identical(poswords,poswords1)
+negwords <- readLines('negative-words.txt')
+sum(!is.na(match(z1,poswords)))
+sum(!is.na(match(z1,negwords)))
+windows()
+wordcloud(z1)
+wordcloud(z1,random.order = T,scale = c(3,0.5), colors = rainbow(8))
 
-
-str(str_split(b,pattern = "\\s+"))
-
-postwords<-scan('positive-words.txt',what = 'character')
-postwords1<-readLines('positive-words.txt')
-identical(postwords,postwords1)
-negwords<-readLines('negative-words.txt')
-sum(!is.na)
